3bc07d2c00bd30a2917d5751475e10d106116bf8
Nguyenduc2317 <nguyenduc2317@gmail.com>
2021-12-19 23:15:50 +0700

    deploy 3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button onClick={()=>{alert('push 2nd')}} type="">login tw</button>
+      <button onClick={()=>{alert('push 2nd push 2nd')}} type="">login tw</button>
     </div>
   );
 }
